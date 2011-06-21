abd72598c8117bbcc26695ede45c16e300f03420
Douglas Freed <dwfreed@mtu.edu>
2011-06-21 08:22:46 -0400

    Defined headers.  Included a .hgignore to exclude build- and development-related files.

---
+++ libircclient_options.h
@@ -0,0 +1,8 @@
+#ifndef LIBIRCCLIENT_OPTIONS_H
+#define LIBIRCCLIENT_OPTIONS_H
+#ifndef IN_LIBIRCCLIENT_H
+	#error This file should not be included directly; include just libircclient.h
+#endif
+#define LIBIRCCLIENT_OPTION_DEBUG		1
+#define LIBIRCCLIENT_OPTION_STRIPNICKS	2
+#endif /* LIBIRCCLIENT_OPTIONS_H */
