baf4adf96e1fbda4dc702c023f18ffbb255fb20c
Douglas Freed <dwfreed@mtu.edu>
2011-06-21 14:41:44 -0400

    Some more headers.

--- global.h
+++ global.h
@@ -1,1 +1,10 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "libircclient.h"
