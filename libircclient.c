4bd858ed8bb7f5008bc40c512336410e35508d79
Doug Freed <931883+dwfreed@users.noreply.github.com>
2011-06-22 11:42:57 -0400

    Wrote irc_run() and supporting functions.  Free incoming_buffer from session in irc_destroy_session().

--- libircclient.c
+++ libircclient.c
@@ -48,6 +48,7 @@ void irc_destroy_session(struct irc_session *session){
 		session->socket = -1;
 	}
 	g_async_queue_unref(session->outgoing_queue);
+	free(session->incoming_buffer);
 	if( session->server ){
 		free(session->server);
 	}
@@ -238,5 +239,339 @@ int irc_connect6(struct irc_session *session, const char *server, unsigned short
 	}
 	session->state = LIBIRCCLIENT_STATE_CONNECTING;
 	session->motd_received = 0;
+	return 0;
+}
+
+inline int irc_is_connected(struct irc_session *session){
+	return session->state == LIBIRCCLIENT_STATE_CONNECTED || session->state == LIBIRCCLIENT_STATE_CONNECTING;
+}
+
+static int irc_add_select_descriptors(struct irc_session *session, fd_set *in_set, fd_set *out_set, int *maxfd){
+	if( session->state == LIBIRCCLIENT_STATE_INIT || session->state == LIBIRCCLIENT_STATE_DISCONNECTED ){
+		session->last_error = LIBIRCCLIENT_ERR_STATE;
+		return 1;
+	}
+	if( session->state == LIBIRCCLIENT_STATE_CONNECTING ){
+		FD_SET(session->socket, out_set);
+		if( session->socket > *maxfd ){
+			*maxfd = session->socket;
+		}
+	} else if( session->state == LIBIRCCLIENT_STATE_CONNECTED ){
+		if( session->incoming_offset < LIBIRCCLIENT_BUFFER_SIZE - 1 ){
+			FD_SET(session->socket, in_set);
+			if( session->socket > *maxfd ){
+				*maxfd = session->socket;
+			}
+		}
+		if( g_async_queue_length(session->outgoing_queue) > 0 ){
+			FD_SET(session->socket, out_set);
+			if( session->socket > *maxfd ){
+				*maxfd = session->socket;
+			}
+		}
+	}
+	return 0;
+}
+
+static int irc_findcrlf(const char *buffer, int length){
+	int offset;
+	for( offset = 0; offset < length - 1; ++offset ){
+		if( buffer[offset] == '\r' && buffer[offset + 1] == '\n' ){
+			return offset + 2;
+		}
+	}
+	return 0;
+}
+
+static void irc_process_incoming_data(struct irc_session *session, size_t process_length){
+	char buffer[511], *p, *s, *command = 0, *prefix = 0;
+	const char **params;
+	int code = 0, param_index = 0;
+	memcpy(buffer, session->incoming_buffer, process_length);
+	buffer[process_length] = '\0';
+	params = (const char **)calloc(16, sizeof(char *));
+	p = buffer;
+	if( buffer[0] == ':' ){
+		for( ; *p && *p != ' '; ++p );
+		*p++ = '\0';
+		prefix = buffer + 1;
+		if( session->options & LIBIRCCLIENT_OPTION_STRIPNICKS ){
+			for( s = prefix; *s; ++s ){
+				if( *s == '@' || *s == '!' ){
+					*s = '\0';
+					break;
+				}
+			}
+		}
+	}
+	if( isdigit(p[0]) && isdigit(p[1]) && isdigit(p[2]) ){
+		p[3] = '\0';
+		code = atoi(p);
+		p += 4;
+	} else {
+		s = p;
+		for( ; *p && *p != ' '; ++p );
+		*p++ = '\0';
+		command = s;
+	}
+	while( *p && param_index < 16 ){
+		if( *p == ':' ){
+			params[param_index++] = ++p;
+			break;
+		}
+		for( s = p; *p && *p != ' '; ++p );
+		params[param_index++] = s;
+		if( *p ){
+			*p++ = '\0';
+		}
+	}
+	if( code ){
+		if( (code == IRC_RFC_RPL_ENDOFMOTD || code == IRC_RFC_ERR_NOMOTD) && !session->motd_received ){
+			session->motd_received = 1;
+			irc_cmd_whois(session, session->nick);
+			if( session->callbacks.event_connect ){
+				session->callbacks.event_connect(session, "CONNECT", prefix, params, param_index);
+			}
+		} else if( code == IRC_RFC_RPL_WHOISUSER ){
+			if(!strcmp(session->nick, params[1])){
+				if(session->hostname){
+					free(session->hostname);
+				}
+				if(session->username){
+					free(session->username);
+				}
+				session->hostname = strdup(params[3]);
+				session->username = strdup(params[2]);
+			}
+		}
+		if( session->callbacks.event_numeric ){
+			session->callbacks.event_numeric(session, code, prefix, params, param_index);
+		}
+	} else {
+		if( !strcmp(command, "PING") && params[0] ){
+			irc_send_raw(session, "PONG %s", params[0]);
+		} else if( !strcmp(command, "NICK") ){
+			char *nick = irc_target_get_nick(prefix);
+			if( !strcmp(nick, session->nick) && params[0] ){
+				free(session->nick);
+				session->nick = strdup(params[0]);
+			}
+			if( session->callbacks.event_nick ){
+				session->callbacks.event_nick(session, command, prefix, params, param_index);
+			}
+		} else if( !strcmp(command, "QUIT") ){
+			if( session->callbacks.event_quit ){
+				session->callbacks.event_quit(session, command, prefix, params, param_index);
+			}
+		} else if( !strcmp(command, "JOIN") ){
+			if( session->callbacks.event_join ){
+				session->callbacks.event_join(session, command, prefix, params, param_index);
+			}
+		} else if( !strcmp(command, "PART") ){
+			if( session->callbacks.event_part ){
+				session->callbacks.event_part(session, command, prefix, params, param_index);
+			}
+		} else if( !strcmp(command, "MODE") ){
+			if( params[0] && !strcmp(params[0], session->nick) ){
+				params[0] = params[1];
+				param_index = 1;
+				if( session->callbacks.event_umode ){
+					session->callbacks.event_umode(session, command, prefix, params, param_index);
+				}
+			} else {
+				if( session->callbacks.event_mode ){
+					session->callbacks.event_mode(session, command, prefix, params, param_index);
+				}
+			}
+		} else if( !strcmp(command, "TOPIC") ){
+			if( session->callbacks.event_topic ){
+				session->callbacks.event_topic(session, command, prefix, params, param_index);
+			}
+		} else if( !strcmp(command, "KICK") ){
+			if( session->callbacks.event_kick ){
+				session->callbacks.event_kick(session, command, prefix, params, param_index);
+			}
+		} else if( !strcmp(command, "PRIVMSG") ){
+			int message_length = strlen(params[1]);
+			if( params[1][0] == '\1' && params[1][message_length - 1] == '\1' ){
+				char ctcp_buffer[512];
+				message_length -= 2;
+				memcpy(ctcp_buffer, params[1] + 1, message_length);
+				ctcp_buffer[message_length] = '\0';
+				params[1] = ctcp_buffer;
+				if( !strcasecmp(params[1], "ACTION ") && session->callbacks.event_ctcp_action ){
+					params[1] = params[1] + 7;
+					param_index = 2;
+					session->callbacks.event_ctcp_action(session, "ACTION", prefix, params, param_index);
+				} else {
+					params[0] = params[1];
+					param_index = 1;
+					if( session->callbacks.event_ctcp_req ){
+						session->callbacks.event_ctcp_req(session, "CTCP", prefix, params, param_index);
+					}
+				}
+			} else if( !strcasecmp(params[0], session->nick) ){
+				if( session->callbacks.event_privmsg ){
+					session->callbacks.event_privmsg(session, command, prefix, params, param_index);
+				}
+			} else {
+				if( session->callbacks.event_channel ){
+					session->callbacks.event_channel(session, command, prefix, params, param_index);
+				}
+			}
+		} else if( !strcmp(command, "NOTICE") ){
+			int message_length = strlen(params[1]);
+			if( params[1][0] == '\1' && params[1][message_length - 1] == '\1' ){
+				char ctcp_buffer[512];
+				message_length -= 2;
+				memcpy(ctcp_buffer, params[1] + 1, message_length);
+				ctcp_buffer[message_length] = '\0';
+				params[0] = ctcp_buffer;
+				param_index = 1;
+				if( session->callbacks.event_ctcp_rep ){
+					session->callbacks.event_ctcp_rep(session, "CTCP", prefix, params, param_index);
+				}
+			} else if( !strcasecmp(params[0], session->nick) ){
+				if( session->callbacks.event_notice ){
+					session->callbacks.event_notice(session, command, prefix, params, param_index);
+				}
+			} else {
+				if( session->callbacks.event_channel_notice ){
+					session->callbacks.event_channel_notice(session, command, prefix, params, param_index);
+				}
+			}
+		} else if( !strcmp(command, "INVITE") ){
+			if( session->callbacks.event_invite ){
+				session->callbacks.event_invite(session, command, prefix, params, param_index);
+			}
+		} else {
+			if( session->callbacks.event_unknown ){
+				session->callbacks.event_unknown(session, command, prefix, params, param_index);
+			}
+		}
+	}
+}
+
+static int irc_process_select_descriptors(struct irc_session *session, fd_set *in_set, fd_set *out_set){
+	if( session->state == LIBIRCCLIENT_STATE_INIT || session->state == LIBIRCCLIENT_STATE_DISCONNECTED ){
+		session->last_error = LIBIRCCLIENT_ERR_STATE;
+		return 1;
+	}
+	if( session->state == LIBIRCCLIENT_STATE_CONNECTING && FD_ISSET(session->socket, out_set) ){
+		struct sockaddr_storage laddr, saddr;
+		socklen_t llen = sizeof(laddr);
+		socklen_t slen = llen;
+		if( getsockname(session->socket, (struct sockaddr *)&laddr, &llen) || getpeername(session->socket, (struct sockaddr *)&saddr, &slen) ){
+			session->last_error = LIBIRCCLIENT_ERR_CONNECT;
+			session->state = LIBIRCCLIENT_STATE_DISCONNECTED;
+			return 1;
+		}
+		if( saddr.ss_family == AF_INET ){
+			memcpy(&session->local_address, &((struct sockaddr_in *)&laddr)->sin_addr, sizeof(struct in_addr));
+			if( session->options & LIBIRCCLIENT_OPTION_DEBUG ){
+				char local_address[INET_ADDRSTRLEN];
+				fprintf(stderr, "[DEBUG] Detected local address: %s\n", inet_ntop(AF_INET, &session->local_address, local_address, INET_ADDRSTRLEN));
+			}
+		} else if( saddr.ss_family == AF_INET6 ){
+			memcpy(&session->local_address, &((struct sockaddr_in6 *)&laddr)->sin6_addr, sizeof(struct in6_addr));
+			if( session->options & LIBIRCCLIENT_OPTION_DEBUG ){
+				char local_address[INET6_ADDRSTRLEN];
+				fprintf(stderr, "[DEBUG] Detected local address: %s\n", inet_ntop(AF_INET6, &session->local_address, local_address, INET6_ADDRSTRLEN));
+			}
+		}
+		session->state = LIBIRCCLIENT_STATE_CONNECTED;
+		if( session->server_password ){
+			irc_send_raw(session, "PASS %s", session->server_password);
+		}
+		irc_send_raw(session, "NICK %s", session->nick);
+		if( !session->username ){
+			session->username = "nobody";
+		}
+		if( !session->realname ){
+			session->realname = "noname";
+		}
+		irc_send_raw(session, "USER %s unknown unknown :%s", session->username, session->realname);
+	}
+	if( session->state != LIBIRCCLIENT_STATE_CONNECTED ){
+		return 1;
+	}
+	if( FD_ISSET(session->socket, in_set) ){
+		int length, readable_length, offset;
+		unsigned int amount;
+		ioctl(session->socket, FIONREAD, &readable_length);
+		session->incoming_buffer = realloc(session->incoming_buffer, session->incoming_offset + readable_length + 1);
+		memset(session->incoming_buffer + session->incoming_offset, 0, readable_length + 1);
+		amount = readable_length;
+		while( (length = recv(session->socket, session->incoming_buffer + session->incoming_offset, amount, 0)) < 0 && errno == EINTR ){
+			continue;
+		}
+		if( length <= 0 ){
+			session->last_error = length == 0 ? LIBIRCCLIENT_ERR_CLOSED : LIBIRCCLIENT_ERR_TERMINATED;
+			session->state = LIBIRCCLIENT_STATE_DISCONNECTED;
+			return 1;
+		}
+		session->incoming_offset += length;
+		while( (offset = irc_findcrlf(session->incoming_buffer, session->incoming_offset)) > 0 ){
+			if( session->options & LIBIRCCLIENT_OPTION_DEBUG ){
+				int i;
+				printf("RECV: ");
+				for( i = 0; i < offset; ++i ){
+					printf("%c", session->incoming_buffer[i]);
+				}
+				printf("\n");
+			}
+			irc_process_incoming_data(session, offset - 2);
+			if( session->incoming_offset - offset ){
+				memmove(session->incoming_buffer, session->incoming_buffer + offset, session->incoming_offset - offset);
+			}
+			session->incoming_offset -= offset;
+			session->incoming_buffer = realloc(session->incoming_buffer, session->incoming_offset);
+		}
+	}
+	if( FD_ISSET(session->socket, out_set) ){
+		int queue_length = g_async_queue_length(session->outgoing_queue);
+		while(queue_length > 0){
+			int length;
+			char *outgoing_message = g_async_queue_pop(session->outgoing_queue);
+			while( (length = send(session->socket, outgoing_message, strlen(outgoing_message), 0)) < 0 && errno == EINTR ){
+				continue;
+			}
+			if( length <= 0 ){
+				session->last_error = length == 0 ? LIBIRCCLIENT_ERR_CLOSED : LIBIRCCLIENT_ERR_TERMINATED;
+				session->state = LIBIRCCLIENT_STATE_DISCONNECTED;
+				return 1;
+			}
+			if( session->options & LIBIRCCLIENT_OPTION_DEBUG ){
+				printf("SEND: %s\n", outgoing_message);
+			}
+			g_free(outgoing_message);
+			queue_length = g_async_queue_length(session->outgoing_queue);
+		}
+	}
+	return 0;
+}
+
+int irc_run(struct irc_session *session){
+	if( session->state != LIBIRCCLIENT_STATE_CONNECTING ){
+		session->last_error = LIBIRCCLIENT_ERR_STATE;
+		return 1;
+	}
+	while( irc_is_connected(session) ){
+		fd_set in_set, out_set;
+		int maxfd = 0;
+		FD_ZERO(&in_set);
+		FD_ZERO(&out_set);
+		irc_add_select_descriptors(session, &in_set, &out_set, &maxfd);
+		if( select(maxfd + 1, &in_set, &out_set, 0, NULL) < 0 ){
+			if( errno == EINTR ){
+				continue;
+			}
+			session->last_error = LIBIRCCLIENT_ERR_TERMINATED;
+			return 1;
+		}
+		if( irc_process_select_descriptors(session, &in_set, &out_set) ){
+			return 1;
+		}
+	}
 	return 0;
 }
