f2a394fea249c5a3ac783705decc75d0bb68c0eb
Doug Freed <931883+dwfreed@users.noreply.github.com>
2011-06-22 11:40:21 -0400

    More headers.

--- global.h
+++ global.h
@@ -1,9 +1,13 @@
 #include <arpa/inet.h>
+#include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
